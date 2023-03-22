f0c66b872b6368d74976a4691b57e888df8393c1
eunsu kim <kimes5376@naver.com>
2023-03-22 11:04:28 +0900

    Initial commit

---
+++ src/main/java/com/example/eunsuptproject/EunsuPtProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.eunsuptproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EunsuPtProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EunsuPtProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eunsuptproject/EunsuPtProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eunsuptproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EunsuPtProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
